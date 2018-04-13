0977b8aa0c912efdca05db2403275691e5f07b15
rgathergood <rgathergood@gmail.com>
2018-04-13 11:10:31 +0100

    wk11 wkd homework - quest complete quests method working

--- week_11/day_05/homework/models/hero.js
+++ week_11/day_05/homework/models/hero.js
@@ -2,12 +2,24 @@ const Hero = function (name, health, favouriteFood) {
   this.name = name;
   this.health = health;
   this.favouriteFood = favouriteFood;
-  this.tasksToComplete = [];
+  this.questsToComplete = [];
+  this.completedQuests = [];
   this.moneyBag = 0;
 }
 
 Hero.prototype.saysHello = function () {
   return (`Hello my name is ${this.name}!`)
+};
+
+Hero.prototype.addQuest = function (quest) {
+  this.questsToComplete.push(quest);
+};
+
+Hero.prototype.completeQuest = function (quest) {
+  quest.markQuestComplete();
+  const index = this.questsToComplete.indexOf(quest);
+  this.questsToComplete.splice(index, 1);
+  this.completedQuests.push(quest);
 };
 
 module.exports = Hero;
--- week_11/day_05/homework/specs/hero_spec.js
+++ week_11/day_05/homework/specs/hero_spec.js
@@ -4,9 +4,13 @@ const Quest = require('../models/quest.js');
 
 describe('Hero', function () {
   let hero;
+  let quest1;
 
   beforeEach( function () {
     hero = new Hero('Boromir', 75, 'Eggs')
+    quest1 = new Quest('Bake a Cake', 4, 'Low', 100)
+    quest2 = new Quest('Kill some orcs', 6, 'Medium', 250)
+    quest3 = new Quest('Save Hobbits', 8, 'High', 400)
   });
 
   it('has a name', function () {
@@ -21,6 +25,14 @@ describe('Hero', function () {
     assert.strictEqual(hero.favouriteFood, 'Eggs')
   });
 
+  it('has questsToComplete', function () {
+    assert.deepStrictEqual(hero.questsToComplete, [])
+  })
+
+  it('has completedQuests', function () {
+    assert.deepStrictEqual(hero.completedQuests, [])
+  })
+
   it('has moneyBag value', function () {
     assert.strictEqual(hero.moneyBag, 0)
   });
@@ -28,5 +40,19 @@ describe('Hero', function () {
   it('can say their name', function () {
     assert.strictEqual(hero.saysHello(), 'Hello my name is Boromir!')
   });
+
+  it('can add quests to questsToComplete', function () {
+    hero.addQuest(quest1);
+    hero.addQuest(quest2);
+    assert.deepStrictEqual(hero.questsToComplete, [quest1, quest2])
+  });
+
+  it('can remove quest', function () {
+    hero.addQuest(quest1);
+    hero.addQuest(quest2);
+    hero.completeQuest(quest2);
+    assert.deepStrictEqual(hero.questsToComplete, [quest1])
+    assert.deepStrictEqual(hero.completedQuests, [quest2])
+  })
 
 });
