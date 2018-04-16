2859e0c89d19e8cea47ec5d34644ed1d82073a12
rgathergood <rgathergood@gmail.com>
2018-04-16 17:54:17 +0100

    wk12 d1 - homework basic CSS - dogs are better

--- week_12/day_01/cats_homework_start_point/public/app.js
+++ week_12/day_01/cats_homework_start_point/public/app.js
@@ -8,7 +8,9 @@ const catsArray = [
   new Cat('Boba', 'Sock fluff', 'http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg'),
   new Cat('Barnaby', 'Tuna', 'https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg'),
   new Cat('Max', 'Whiskas Temptations', 'http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg'),
-  new Cat('Cheshire', 'Caterpillars', 'https://i0.wp.com/chartcons.com/wp-content/uploads/cheshire-2.png?resize=1021%2C580&ssl=1')
+  new Cat('Cheshire', 'Caterpillars', 'https://i0.wp.com/chartcons.com/wp-content/uploads/cheshire-2.png?resize=1021%2C580&ssl=1'),
+  new Cat('Jackie', 'Cats', "images/IMG8.JPG")
+
 ]
 
 const addCat = function (cat, container) {
@@ -16,18 +18,20 @@ const addCat = function (cat, container) {
 
   const name = document.createElement('li');
   name.textContent = `Name: ${cat.name}`;
-  cats.appendChild(name);
 
   const favouriteFood = document.createElement('li');
   favouriteFood.textContent = `Favourite food: ${cat.favouriteFood}`;
-  cats.appendChild(favouriteFood);
 
+  const imgLi = document.createElement('li');
   const image = document.createElement('img');
   image.src = cat.imageSource;
   image.width = "500";
-  cats.appendChild(image);
 
   container.appendChild(cats);
+  cats.appendChild(name);
+  cats.appendChild(favouriteFood);
+  cats.appendChild(imgLi);
+  cats.appendChild(image);
 }
 
 document.addEventListener('DOMContentLoaded', () => {
