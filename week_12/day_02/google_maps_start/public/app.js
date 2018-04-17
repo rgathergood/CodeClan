a58547ec4f694972ef140b0af2b424bf5f31f38a
rgathergood <rgathergood@gmail.com>
2018-04-17 15:41:54 +0100

    wk12 d2 - Map Markers Added

--- week_12/day_02/google_maps_start/public/app.js
+++ week_12/day_02/google_maps_start/public/app.js
@@ -1,7 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
   const mapContainer = document.querySelector('#main-map');
 
-  const center = {lat: 55.946962, lng:-3.201958};
-  const mainMap = new MapWrapper(mapContainer, center, 15);
+  const codeclanE = {lat: 55.946962, lng:-3.201958};
+  const codeclanG = {lat:55.865477, lng:-4.257998}
+  const mainMap = new MapWrapper(mapContainer, codeclanE, 15);
+
+  mainMap.addMarker(codeclanE);
+  mainMap.addMarker(codeclanG);
 
 });
--- week_12/day_02/google_maps_start/public/map_wrapper.js
+++ week_12/day_02/google_maps_start/public/map_wrapper.js
@@ -1,8 +1,13 @@
 const MapWrapper = function (container, center, zoom) {
-
   this.googleMap = new google.maps.Map(container, {
     center: center,
     zoom: zoom
   });
+}
 
+MapWrapper.prototype.addMarker = function (coords) {
+  const marker = new google.maps.Marker({
+    map: this.googleMap,
+    position: coords
+  });
 }
