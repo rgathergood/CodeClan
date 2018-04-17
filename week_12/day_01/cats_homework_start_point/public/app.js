84355efb3cecbea409409ab75b48dd9a8d4a8e7c
rgathergood <rgathergood@gmail.com>
2018-04-17 09:31:51 +0100

    wk12 d1 - homework minor edits after review

--- week_12/day_01/cats_homework_start_point/public/app.js
+++ week_12/day_01/cats_homework_start_point/public/app.js
@@ -10,7 +10,6 @@ const catsArray = [
   new Cat('Max', 'Whiskas Temptations', 'http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg'),
   new Cat('Cheshire', 'Caterpillars', 'https://i0.wp.com/chartcons.com/wp-content/uploads/cheshire-2.png?resize=1021%2C580&ssl=1'),
   new Cat('Jackie', 'Cats', "images/IMG8.JPG")
-
 ]
 
 const addCat = function (cat, container) {
@@ -25,13 +24,12 @@ const addCat = function (cat, container) {
   const imgLi = document.createElement('li');
   const image = document.createElement('img');
   image.src = cat.imageSource;
-  image.width = "500";
 
   container.appendChild(cats);
   cats.appendChild(name);
   cats.appendChild(favouriteFood);
   cats.appendChild(imgLi);
-  cats.appendChild(image);
+  imgLi.appendChild(image);
 }
 
 document.addEventListener('DOMContentLoaded', () => {
