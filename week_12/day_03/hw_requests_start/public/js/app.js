f1e1235f87eb5e262034b2fe6a8827cedb2557c2
rgathergood <rgathergood@gmail.com>
2018-04-18 17:31:51 +0100

    week12 d3 - homework - mvp complete

--- week_12/day_03/hw_requests_start/public/js/app.js
+++ week_12/day_03/hw_requests_start/public/js/app.js
@@ -38,7 +38,6 @@ const handleBeerSelection = function (event) {
   displayBeerInformation(beer);
 }
 
-
 const displayBeerInformation = function (beer) {
   const mainDiv = document.querySelector('#beer-info');
   mainDiv.innerHTML = ''
@@ -47,18 +46,45 @@ const displayBeerInformation = function (beer) {
   const beerName = document.createElement('h2');
   const beerImg = document.createElement('img');
   const beerTagline = document.createElement('h5');
+  const beerFoodPairing = document.createElement('h6');
+  const beerBrewersTips = document.createElement('h6');
   const beerDescription = document.createElement('h5');
   const ingredientsDiv = document.createElement('div');
+  const ingredientsDivTitle = document.createElement('h4');
+  const malts = document.createElement('h5');
+  const hops = document.createElement('h5');
+  const yeast = document.createElement('h5');
 
   beerName.textContent = beer.name;
   beerImg.src = beer.image_url;
   beerTagline.textContent = beer.tagline;
   beerDescription.textContent = beer.description;
+  beerFoodPairing.textContent = `Food Pairings: ${beer.food_pairing}`;
+  beerBrewersTips.textContent = `Brewers Tips: ${beer.brewers_tips}`;
+  ingredientsDivTitle.textContent = 'Ingredients';
+
+
+  const maltNames =[]
+  beer.ingredients.malt.forEach((malt) => maltNames.push(malt.name));
+  malts.textContent = `Malt: ${maltNames.join(", ")}`;
+
+  const hopNames =[]
+  beer.ingredients.hops.forEach((hop) => hopNames.push(hop.name));
+  hops.textContent = `Hops: ${hopNames.join(", ")}`;
+
+  yeast.textContent = `Yeast: ${beer.ingredients.yeast}`
+
 
   mainDiv.appendChild(div);
   div.appendChild(beerName);
   div.appendChild(beerImg);
   div.appendChild(beerTagline);
   div.appendChild(beerDescription);
+  div.appendChild(beerFoodPairing);
+  div.appendChild(beerBrewersTips);
   div.appendChild(ingredientsDiv);
+  ingredientsDiv.appendChild(ingredientsDivTitle);
+  ingredientsDiv.appendChild(malts);
+  ingredientsDiv.appendChild(hops);
+  ingredientsDiv.appendChild(yeast);
 }
