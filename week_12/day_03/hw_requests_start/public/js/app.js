26be933c13a77cdd10412e7643bac22753fa1a35
rgathergood <rgathergood@gmail.com>
2018-04-18 16:52:50 +0100

    week12 d3 - homework - dropdown added

--- week_12/day_03/hw_requests_start/public/js/app.js
+++ week_12/day_03/hw_requests_start/public/js/app.js
@@ -3,6 +3,9 @@ let beers = [];
 document.addEventListener('DOMContentLoaded', () => {
   const url = 'https://api.punkapi.com/v2/beers';
   makeRequest(url, requestComplete);
+
+  const select = document.querySelector('#beer-list');
+  select.addEventListener('change', handleBeerSelection)
 });
 
 const makeRequest = function (url, callback) {
@@ -16,21 +19,36 @@ const requestComplete = function () {
   if (this.status != 200) return;
   const jsonString = this.responseText;
   beers = JSON.parse(jsonString);
-  loopBeers();
+  populateList(beers);
 }
 
-const loopBeers = function () {
-  beers.forEach((beer) => populateList(beer));
+const populateList = function (beers) {
+  const select = document.querySelector('#beer-list');
+  beers.forEach((beer, index) => {
+    const option = document.createElement('option');
+    option.textContent = beer.name;
+    const value = index;
+    option.value = value;
+    select.appendChild(option);
+  });
 }
 
-const populateList = function (beer) {
-  const mainDiv = document.querySelector('#beer-list');
+const handleBeerSelection = function (event) {
+  const beer = beers[this.value]
+  displayBeerInformation(beer);
+}
+
+
+const displayBeerInformation = function (beer) {
+  const mainDiv = document.querySelector('#beer-info');
+  mainDiv.innerHTML = ''
 
   const div = document.createElement('div');
   const beerName = document.createElement('h2');
   const beerImg = document.createElement('img');
   const beerTagline = document.createElement('h5');
   const beerDescription = document.createElement('h5');
+  const ingredientsDiv = document.createElement('div');
 
   beerName.textContent = beer.name;
   beerImg.src = beer.image_url;
@@ -42,4 +60,5 @@ const populateList = function (beer) {
   div.appendChild(beerImg);
   div.appendChild(beerTagline);
   div.appendChild(beerDescription);
+  div.appendChild(ingredientsDiv);
 }
