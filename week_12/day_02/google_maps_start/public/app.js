f309e6755b7e000ba0e110201bbb056980977293
rgathergood <rgathergood@gmail.com>
2018-04-18 08:02:15 +0100

    wk12 d2 - homework - InfoWindow Added

--- week_12/day_02/google_maps_start/public/app.js
+++ week_12/day_02/google_maps_start/public/app.js
@@ -5,7 +5,20 @@ document.addEventListener('DOMContentLoaded', () => {
   const codeclanG = {lat:55.865477, lng:-4.257998}
   const mainMap = new MapWrapper(mapContainer, codeclanE, 15);
 
-  mainMap.addMarker(codeclanE);
+  const contentString = '<div id="content">'+
+  '<div id="siteNotice">'+
+  '</div>'+
+  '<h1 id="firstHeading" class="firstHeading">CodeClan</h1>'+
+  '<div id="bodyContent">'+
+  '<p>At <b>CodeClan</b>, we are passionate about teaching digital skills'+
+  'including software development and user experience design. We believe'+
+  'that promoting digital excellence creates opportunities for our students,'+
+  'our graduates, our employer partners and the wider economy.</p>'+
+  '</div>'+
+  '</div>';
+
+  mainMap.addMarker(codeclanE, contentString);
   mainMap.addMarker(codeclanG);
 
+  mainMap.addClickListener();
 });
--- week_12/day_02/google_maps_start/public/map_wrapper.js
+++ week_12/day_02/google_maps_start/public/map_wrapper.js
@@ -3,11 +3,33 @@ const MapWrapper = function (container, center, zoom) {
     center: center,
     zoom: zoom
   });
+
+  this.markers = [];
 }
 
-MapWrapper.prototype.addMarker = function (coords) {
+MapWrapper.prototype.addMarker = function (coords, contentString, icon) {
   const marker = new google.maps.Marker({
     map: this.googleMap,
-    position: coords
+    position: coords,
+    icon: icon
+  });
+
+  const infoWindow = new google.maps.InfoWindow ({
+    content: contentString,
+    maxWidth: 300
+  });
+
+  marker.addListener('click', function () {
+    infoWindow.open(this.googleMap, marker);
+  });
+
+  this.markers.push(marker); //keeps track of all markers
+}
+
+MapWrapper.prototype.addClickListener = function () {
+  google.maps.event.addListener(this.googleMap, 'click', (event) => { //arrow function needed here for context (this.addMarker is not a function)
+    const lat = event.latLng.lat();
+    const lng = event.latLng.lng();
+    this.addMarker({lat, lng});
   });
 }
