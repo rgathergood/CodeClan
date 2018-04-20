21e225339cf635df9ef904a17cd7383bc5401c1f
rgathergood <rgathergood@gmail.com>
2018-04-20 16:24:20 +0100

    images added

--- week_12/day_05/homework/src/views/planet_view.js
+++ week_12/day_05/homework/src/views/planet_view.js
@@ -14,9 +14,54 @@ PlanetView.prototype.renderSelect = function (planetData) {
 
 PlanetView.prototype.renderDetail = function (planet) {
   this.planetContainer.innerHTML = "";
+
+  function prettify(str) {
+    return str.replace(/(-|^)([^-]?)/g, function(_, prep, letter) {
+      return (prep && ' ') + letter.toUpperCase();
+    });
+  }
+
+  function commafy( num ) {
+    var str = num.toString().split('.');
+    if (str[0].length >= 5) {
+      str[0] = str[0].replace(/(\d)(?=(\d{3})+$)/g, '$1,');
+    }
+    if (str[1] && str[1].length >= 5) {
+      str[1] = str[1].replace(/(\d{3})/g, '$1 ');
+    }
+    return str.join('.');
+  }
+
   const planetName = document.createElement('p');
   planetName.textContent = planet.name;
   this.planetContainer.appendChild(planetName);
+
+  console.log(planet);
+
+  const planetImg = document.createElement('img');
+  planetImg.src = `../../public/images/${planet.name}.png`;
+  this.planetContainer.appendChild(planetImg);
+
+  const planetClimate = document.createElement('p');
+  displayClimate = prettify(planet.climate);
+  planetClimate.textContent = 'Climate: ' + displayClimate;
+  this.planetContainer.appendChild(planetClimate);
+
+  const planetTerrain = document.createElement('p');
+  const terrains = planet.terrain.replace(/\b\w/g, l => l.toUpperCase())
+  planetTerrain.textContent = 'Terrain: ' + terrains;
+  this.planetContainer.appendChild(planetTerrain);
+
+  const planetPopulation = document.createElement('p');
+  const population = planet.population;
+  if (population === 'unknown') {
+    prettify(population)}
+    else {
+      commafy(population);
+    }
+  planetPopulation.textContent = 'Population: ' + population;
+  this.planetContainer.appendChild(planetPopulation);
+
 };
 
 module.exports = PlanetView;
