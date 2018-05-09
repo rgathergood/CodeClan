dffb237c89c73bb7fbcc408f44e64d4f9491c607
Rob <rgathergood@gmail.com>
2018-05-09 08:24:27 +0100

    wk15 d2 homwork - images added

--- week_15/day_02/top20/src/components/SongList.js
+++ week_15/day_02/top20/src/components/SongList.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SongList = ({songs}) => {
   var songNodes = songs.map((song, index) => {
-    return <li key={index}>{song['im:name'].label} {song['im:artist'].label}</li>
+    return <li key={index}>{song['im:name'].label} {song['im:artist'].label} <img alt='cover' src={song['im:image'][0].label}/></li>
   });
 
   return (
